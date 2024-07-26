2bc5c757ac0af4265c8c9c786aa88459e2c9f2fb
Bogdan Vlad <vladbgd@gmail.com>
2024-07-26 12:38:04 +0300

    Add logs to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,29 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-pwd 
-docker buildx build --load --platform linux/amd64 -f Dockerfile.dev  --no-cache -t genezio-build-$1 .
+# Print working directory
+echo "Current directory: $(pwd)"
 
-docker tag genezio-build-$1:latest harbor-registry.prod.cluster.genez.io/genezio/genezio-build-$1:latest
+# Build the Docker image
+echo "Building Docker image..."
+docker buildx build --load --platform linux/amd64 -f Dockerfile.dev --no-cache -t genezio-build-$1:latest .
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 408878048420.dkr.ecr.us-east-1.amazonaws.com/genezio-build-$1
+# Verify the Docker image exists locally
+if [[ "$(docker images -q genezio-build-$1:latest 2> /dev/null)" == "" ]]; then
+  echo "Docker image genezio-build-$1:latest was not found locally. Build failed."
+  exit 1
+fi
 
+# Tag the Docker image
+echo "Tagging Docker image..."
+docker tag genezio-build-$1:latest 408878048420.dkr.ecr.us-east-1.amazonaws.com/genezio-build-$1:latest
+
+# Log in to AWS ECR
+echo "Logging into AWS ECR..."
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 408878048420.dkr.ecr.us-east-1.amazonaws.com
+
+# Push the Docker image to ECR
+echo "Pushing Docker image to ECR..."
 docker push 408878048420.dkr.ecr.us-east-1.amazonaws.com/genezio-build-$1:latest
+
+echo "Docker image pushed successfully."
