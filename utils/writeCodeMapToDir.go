7ab3a5f28c2c7fb85de899a815c5bf90dcd81634
Bogdan Vlad <vladbgd@gmail.com>
2024-08-07 14:41:56 +0300

    Code formatting.

--- utils/writeCodeMapToDir.go
+++ utils/writeCodeMapToDir.go
@@ -10,31 +10,31 @@ import (
 
 func WriteCodeMapToDirAndZip(code map[string]string, tmpFolderPath string) (string, error) {
 	// Write code to temp folder
-    for fileName, fileContent := range code {
-        filePath := path.Join(tmpFolderPath, fileName)
-        log.Default().Println("Writing file", fileName, "to", tmpFolderPath)
+	for fileName, fileContent := range code {
+		filePath := path.Join(tmpFolderPath, fileName)
+		log.Default().Println("Writing file", fileName, "to", tmpFolderPath)
 
-        // Check if file is in a subfolder
-        if strings.Contains(fileName, "/") {
-            log.Println("Creating subfolder", path.Dir(filePath))
-            err := os.MkdirAll(path.Dir(filePath), 0755)
-            if err != nil {
-                return "", err
-            }
-        }
+		// Check if file is in a subfolder
+		if strings.Contains(fileName, "/") {
+			log.Println("Creating subfolder", path.Dir(filePath))
+			err := os.MkdirAll(path.Dir(filePath), 0755)
+			if err != nil {
+				return "", err
+			}
+		}
 
-        decoded, err := base64.StdEncoding.DecodeString(fileContent)
-        fileBytes := []byte(fileContent)
+		decoded, err := base64.StdEncoding.DecodeString(fileContent)
+		fileBytes := []byte(fileContent)
 
-        if err == nil {
-            fileBytes = decoded
-        } 
+		if err == nil {
+			fileBytes = decoded
+		}
 
-        err = os.WriteFile(filePath, fileBytes, 0644)
-        if err != nil {
-            return "", err
-        }
-    }
+		err = os.WriteFile(filePath, fileBytes, 0644)
+		if err != nil {
+			return "", err
+		}
+	}
 
 	destinationPath := path.Join(tmpFolderPath, "projectCode.zip")
 
